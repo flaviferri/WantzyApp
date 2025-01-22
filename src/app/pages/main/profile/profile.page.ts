5046a6e655c44bde72c86a1c22303776a70bea37
flaviferri <flaviferri90@gmail.com>
2025-01-22 23:59:25 +0100

    avatar updated

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { User } from 'src/app/models/userModel';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 @Component({
@@ -12,9 +15,60 @@ import { HeaderComponent } from 'src/app/shared/components/header/header.compone
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+ firebaseSvc = inject(FirebaseService);
+    utilSvc = inject(UtilsService);
 
   ngOnInit() {
   }
 
+   user(): User{
+      return this.utilSvc.getFromLocalStorage('user')
+    }
+
+      /*   ======= TAKE / SELECT IMAGE ======
+   */
+
+  async takeImage() {
+    let user = this.user();
+
+    let path = `users/${user.uid}`;
+
+
+    const dataUrl = (await this.utilSvc.takePicture('Profile image')).dataUrl;
+    const loading = await this.utilSvc.loading();
+    await loading.present();
+
+    let imagePath = `${user.uid}/profile}`;
+    user.image = await this.firebaseSvc.uploadImage(imagePath, dataUrl);
+
+    this.firebaseSvc
+    .updateDocument(path, {image: user.image})
+    .then(async (res) => {
+this.utilSvc.saveInLocalStorage('user',user);
+      this.utilSvc.presentToast({
+        message: 'image updated successfully',
+        duration: 1500,
+        color: 'sucess',
+        position: 'middle',
+        icon: 'checkmark-circle-outline',
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+
+      this.utilSvc.presentToast({
+        message: error.message,
+        duration: 2500,
+        color: 'primary',
+        position: 'middle',
+        icon: 'alert-circle',
+      });
+    })
+    .finally(() => {
+      loading.dismiss();
+    });
 }
+
+  }
+
+
