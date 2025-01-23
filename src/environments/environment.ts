46c14ca1a2b6e630c3d664bd16cfbe9c2e0e7381
Flavia Ferrigno <163913487+flaviferri@users.noreply.github.com>
2025-01-23 18:05:01 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,14 @@
 
 export const environment = {
   production: false,
-
-  firebaseConfig :{
-    apiKey: "AIzaSyDdNO_tgeaxDzw0e9TQisvNr0JtluTh9as",
-    authDomain: "product-wantzy-app.firebaseapp.com",
-    projectId: "product-wantzy-app",
-    storageBucket: "product-wantzy-app.firebasestorage.app",
-    messagingSenderId: "579974815663",
-    appId: "1:579974815663:web:4a50307bb0d645d727995a"
-  }
+firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+}
 };
 
 /*
